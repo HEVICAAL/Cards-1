ebbef1dc3e87d2dd096e5992d2b3a7e655adae0a
HEVICAAL <chernandez101@ucol.mx>
2025-10-02 23:08:59 -0600

    Añadir .gitignore y desplegar sitio Blazor

--- Cards/obj/Release/net8.0/Cards.AssemblyInfo.cs
+++ Cards/obj/Release/net8.0/Cards.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cards")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8050f09e325fb49225784f86e3d7afbc4fb2bf2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ddc37c86bfdd3401a7ecc324fb0f8063642099")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cards")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cards")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
